17f367344ed0f24ccc615f49d7e7853ca0bf41d0
elcoruco <el.coruco@gmail.com>
2022-10-05 12:48:22 -0500

    add setup to svg props

--- src/BarchartView.vue
+++ src/BarchartView.vue
@@ -2,14 +2,20 @@
 import { ref, computed, onMounted/*, watch*/ } from "vue";
 import { format } from "d3-format";
 const margin     = ref();
-const height     = ref();
+const defaultHeight = ref(400);
+const defaultWidth  = ref(400);
 const xLabel     = ref();
 const yLabel     = ref();
 const minWidth   = ref();
 const barWidth   = ref();
 const color      = ref();
-const background = ref();
+const defaultBackground = ref("white");
 const ticks      = ref();
+
+const props      = defineProps({ width : Number, height : Number});
+const width      = computed( () => props.width || defaultWidth.value)
+const height     = computed( () => props.height || defaultHeight.value)
+const background = computed( () => props.background || defaultBackground.value)
 
 const f          = format(",");
 </script>
@@ -18,6 +24,18 @@ const f          = format(",");
     <h1>
       <slot name="title" />
     </h1>
-    <svg></svg>
+    <svg
+      ref="svg"
+      xmlns="http://www.w3.org/2000/svg"
+      :style="{ background: background }"
+      version="1.2"
+      baseProfile="tiny"
+      width="100%"
+      height="100%"
+      :viewBox="`0 0 ${width} ${height}`"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+      class="gf_barchart_svg">
+    </svg>
   </div>
 </template>
